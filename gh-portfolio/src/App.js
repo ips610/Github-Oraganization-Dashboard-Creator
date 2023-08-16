bb1f3b83c3cf4d1927aaff144cd7493a7f29cd0c
Mankirat Singh <msingh2_be22@thapar.edu>
2023-08-16 18:10:34 +0530

    mix content error resolved ig
    Signed-off-by: Mankirat Singh <msingh2_be22@thapar.edu>

--- gh-portfolio/src/App.js
+++ gh-portfolio/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HomePage } from './homePage/homePage';
 import Owasp from './owasp/owasp';
 import Routing from './all route/route';
+import DocumentMeta from 'react-document-meta';
 
 function App() {
   return (
   <Router>
+    <DocumentMeta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
     <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/owasp" element={<Owasp/>} />
